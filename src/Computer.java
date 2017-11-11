7929eea1867c5243aced6f6c2834ace409bfb3be
tiany4 <tiany4@vcu.edu>
2017-11-11 01:07:59 -0500

    bullshit

--- src/Computer.java
+++ src/Computer.java
@@ -1,12 +1,14 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Computer {
     CPU cpu = new CPU();
     MainMemory memory = new MainMemory();
     Scheduler rrScheduler = new Scheduler(memory);
     Dispatcher dispatcher = new Dispatcher(cpu, memory);
+    ProcessGenerator progen = new ProcessGenerator();
     private int processid = 0;
 
     public Computer() {
@@ -50,8 +52,46 @@ public class Computer {
         return dispatcher.displayProcesses();
     }
 
-    public int getAmount(){
+    public String gen(int numberOfProcesses) {
+        for (int i = 0; i < numberOfProcesses; i++) {
+            progen.gen();
+        }
+        return "Process generation complete.";
+    }
+
+    public int getAmount() {
         return dispatcher.getAmount();
     }
 
+    private class ProcessGenerator {
+
+        public ProcessGenerator() {
+        }
+
+        private void wordProcessor() {
+            String name = "word" + processid;
+            int memoryRequirement;
+            int burstCycle;
+            int priority = 3;
+            int totalCycleCount;
+            int ioCycle;
+            dispatch(name, asrgfae4rgfr;k);
+        }
+
+        private void gen() {
+            int randomInt = ThreadLocalRandom.current().nextInt(5);
+            switch (randomInt) {
+                case 1: wordProcessor();
+                case 2:
+            }
+        }
+
+        private void dispatch(String name, int memoryRequirement, int burstCycle, int priority,
+                              int totalCycleCount, int ioCycle, int yieldCycle) {
+            PCB newProcess = new PCB(name, processid, memoryRequirement, burstCycle, priority, totalCycleCount,
+                    ioCycle, yieldCycle);
+            dispatcher.dispatch(newProcess);
+            processid++;
+        }
+    }
 }
